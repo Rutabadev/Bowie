1ad955aaf670e13c3afa920b48fa585aa9d088ea
Puex <montaille.matthieu@gmail.com>
2016-11-22 21:59:50 +0100

    Update

--- js/scroll.js
+++ js/scroll.js
@@ -18,3 +18,18 @@ jQuery(document).ready(function () {
         return false;
     })
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+  });
+});
